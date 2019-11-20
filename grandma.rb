2728704d14a95efbdd9848df420abc17f80d6f62
Craig Storer <craigstorer@gmail.com>
2019-11-20 15:32:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
      "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.capitalize
      "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+  elsif phrase == phrase.upcase
      "NO, NOT SINCE 1938!"
   elsif phrase == phrase
      "HUH?! SPEAK UP, SONNY!"
